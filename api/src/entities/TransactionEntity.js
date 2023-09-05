8ad8ff59e211bbeae16439666fc1050f13ba06c5
igor-magno <igormagno.dev@gmail.com>
2023-09-05 19:46:24 -0300

    feat: :label: Create entities Transaction and Type

---
+++ api/src/entities/TransactionEntity.js
@@ -0,0 +1,13 @@
+class TransactionEntity
+{
+    constructor(type, date, product, value, seller)
+    {
+        this.type = type
+        this.date = date
+        this.product = product
+        this.value = value
+        this.seller = seller
+    }
+}
+
+export default TransactionEntity
---
+++ api/src/entities/TypeEntity.js
@@ -0,0 +1,12 @@
+class TypeEntity
+{
+    constructor(type, description, nature, operator)
+    {
+        this.type = type
+        this.description = description
+        this.nature = nature
+        this.operator = operator
+    }
+}
+
+export default TypeEntity
