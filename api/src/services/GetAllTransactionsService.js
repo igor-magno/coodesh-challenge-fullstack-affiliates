924c78196215badf720b9e4d06c23ef7cc8da794
igor-magno <igormagno.dev@gmail.com>
2023-09-06 21:57:06 -0300

    feat: :construction: Get the total amount of transactions
    Implements service

--- api/src/services/GetAllTransactionsService.js
+++ api/src/services/GetAllTransactionsService.js
@@ -1,6 +1,14 @@
 class GetAllTransactionsService
 {
+    constructor({ transactionRepository })
+    {
+        this.transactionRepository = transactionRepository
+    }
 
+    async run()
+    {
+        return await this.transactionRepository.all()
+    }
 }
 
 export default GetAllTransactionsService
