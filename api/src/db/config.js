1930246cc50e85fb4cb7c269a715a1ba12e356ec
igor-magno <igormagno.dev@gmail.com>
2023-09-06 21:50:50 -0300

    build: :wrench: Add prefix DEV and TEST in the environment variables that configure the bunch the connection to db

--- api/src/db/config.js
+++ api/src/db/config.js
@@ -1,22 +1,22 @@
 export default {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_CONNECTION,
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_DATABASE,
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    dialect: process.env.DEV_DB_CONNECTION,
     dialectOptions: {
       bigNumberStrings: true
     }
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_CONNECTION,
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_DATABASE,
+    host: process.env.TEST_DB_HOST,
+    port: process.env.TEST_DB_PORT,
+    dialect: process.env.TEST_DB_CONNECTION,
     dialectOptions: {
       bigNumberStrings: true
     }
