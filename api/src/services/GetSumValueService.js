725bfbb4817653e17ae8c094e29d374588f16e38
igor-magno <igormagno.dev@gmail.com>
2023-09-06 21:58:11 -0300

    feat: :construction: Get the total amount of transactions
    Implement service

--- api/src/services/GetSumValueService.js
+++ api/src/services/GetSumValueService.js
@@ -1,6 +1,14 @@
 class GetSumValueService
 {
+    constructor({ transactionRepository })
+    {
+        this.transactionRepository = transactionRepository
+    }
 
+    async run()
+    {
+        return await this.transactionRepository.getTotalValue()
+    }
 }
 
 export default GetSumValueService
