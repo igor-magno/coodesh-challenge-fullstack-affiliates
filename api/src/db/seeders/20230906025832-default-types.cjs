a23388afca4c5be6127d5ad19b94ff0a7e21b48f
igor-magno <igormagno.dev@gmail.com>
2023-09-06 21:51:59 -0300

    style: Code formatting

--- api/src/db/seeders/20230906025832-default-types.cjs
+++ api/src/db/seeders/20230906025832-default-types.cjs
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Types', [{
       id: 1,
       description: 'Venda produtor',
@@ -10,24 +10,21 @@ module.exports = {
       operator: '+',
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
-    await queryInterface.bulkInsert('Types', [{
+    }, {
       id: 2,
       description: 'Venda afiliado',
       nature: 'Entrada',
       operator: '+',
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
-    await queryInterface.bulkInsert('Types', [{
+    }, {
       id: 3,
       description: 'Comissão paga',
       nature: 'Saída',
       operator: '-',
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
-    await queryInterface.bulkInsert('Types', [{
+    }, {
       id: 4,
       description: 'Comissão recebida',
       nature: 'Entrada',
@@ -37,7 +34,7 @@ module.exports = {
     }]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Types', null, {});
   }
 };
