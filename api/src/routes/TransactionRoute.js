78a4083014ef65c3d213895582e2b7c9a030be1d
igor-magno <igormagno.dev@gmail.com>
2023-09-08 19:57:19 -0300

    fix: Configuring the api to not have to use no-cors in the web client

--- api/src/routes/TransactionRoute.js
+++ api/src/routes/TransactionRoute.js
@@ -14,28 +14,35 @@ const routes = {
             typeRepository: new TypeRepository({ typeModel: TypeModel })
         })).run(request)
 
-        response.writeHead(200, {
-            'content-type': 'text/plain'
+        await response.writeHead(200, {
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': '*'
         })
 
-        return response.end('O arquivo foi importado com sucesso, E todas as transações já estão disponíveis na listagem principal')
+        response.write('O arquivo foi importado com sucesso, E todas as transações já estão disponíveis na listagem principal')
+
+        return response.end()
     },
     '/transaction:get': async (request, response) => {
         
         const result = await (new GetAllTransactionsService({ transactionRepository: new TransactionRepository({ transactionModel: TransactionModel }) })).run()
         
-        response.writeHead(200, {
-            'content-type': 'aplication/json'
+        await response.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         })
         
-        return response.end(JSON.stringify(result))
+        await response.write(JSON.stringify(result))
+        
+        return response.end()
     },
     '/transaction/sum-value:get': async (request, response) => {
 
         const result = await (new GetSumValueService({ transactionRepository: new TransactionRepository({ transactionModel: TransactionModel }) })).run()
         
         response.writeHead(200, {
-            'content-type': 'aplication/json'
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         })
         
         return response.end(JSON.stringify({
