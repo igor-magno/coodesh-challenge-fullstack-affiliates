97d16b0e4741661fdc7722d4cb57274d29d436fa
igor-magno <igormagno.dev@gmail.com>
2023-09-08 19:55:38 -0300

    fix: The getCode error method is not defined

--- api/src/handler.js
+++ api/src/handler.js
@@ -31,8 +31,12 @@ const handler = (request, response) => {
 
 const handlerError = (response) => {
     return error => {
-        response.writeHead(error.getCode())
-        return response.end(error.getMessage())
+        let code = 500
+        let message = error.message
+        if('getCode' in error) code = error.getCode()
+        if('getMessage' in error) message = error.getMessage()
+        response.writeHead(code)
+        return response.end(message)
     }
 }
 
